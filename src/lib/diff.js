dae59070babd7ce99069de83c350dc0233c094fb
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2017-12-22 17:04:52 -0500

    # Conflicts: #	demo/demo.js #	lib/diff.js #	package.json #	src/lib/diff.js

--- src/lib/diff.js
+++ src/lib/diff.js
@@ -2,14 +2,12 @@ import compare from './compare/compare';
 
 function diff(oldState, newState){
     const comparisonValue = compare(oldState,newState);
-    if (comparisonValue === -1)
-        return oldState;
-
-    if (comparisonValue === 1)
-        return newState
-
-    if (comparisonValue === 0)
+    if (comparisonValue === 0){
         return undefined
+    }
+    else{
+        return newState;
+    }
 }
 
 export default diff;
